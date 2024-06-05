08f31d54a4d1c82363f5b60c4107ea7e4e98137b
Martin Garcia <Martin_g-garcia@hotmail.com>
2024-06-05 18:56:52 -0300

    Instalacion de firebase, tanto por npm como por insercion de archivo

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwlQ9GvcPMcE8FD3RKL_UIWmWncun7Zlw",
+  authDomain: "black-knight-react.firebaseapp.com",
+  projectId: "black-knight-react",
+  storageBucket: "black-knight-react.appspot.com",
+  messagingSenderId: "1002557251647",
+  appId: "1:1002557251647:web:b0333229a706db1cac588a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
